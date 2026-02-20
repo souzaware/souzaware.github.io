debfaf7d58db21841aa33626ec3c123afbd92f37
Andrei <71538656+souzaware@users.noreply.github.com>
2026-02-20 18:52:35 +0000

    check if request was successful before setting latest video id

--- static/res/scripts/fetchLatestVideo.js
+++ static/res/scripts/fetchLatestVideo.js
@@ -3,7 +3,10 @@ var id = "dQw4w9WgXcQ";
 (async () => {
     try {
         const req = await fetch("LATEST_VIDEO_ID");
-        id = await req.text();
+
+        if (req.status.ok) {
+            id = await req.text();
+        }
     } catch (e) {}
 
     latest_video_embed.src = `https://youtube.com/embed/${id}`;
